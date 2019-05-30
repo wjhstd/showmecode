3ab01906682571f79ce940db53924edf7bddc313
wangjunhui <junhui.wang@kaixiangtech.com>
2019-05-30 18:03:21 +0800

    整理

--- code-rpc/src/main/java/cn/a52going/coderpc/dto/input/baseinput/CmsInput.java
+++
@@ -1,9 +0,0 @@
-package cn.a52going.coderpc.dto.input.baseinput;
-/**
- *@ClassName: CmsInput
- *@description: 类描述
- *@author: wangjunhui@wondersgroup.com
- *@data: 2018/4/13 14:38
- **/
-public interface CmsInput {
-}
--- code-rpc/src/main/java/cn/a52going/coderpc/model/basemodel/BaseModel.java
+++
@@ -1,9 +0,0 @@
-package cn.a52going.coderpc.model.basemodel;
-
-import java.io.Serializable;
-
-public interface BaseModel<ID extends Serializable> {
-    ID getId();
-
-    String getOrderStr();
-}
